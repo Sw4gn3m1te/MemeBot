4e67c6f6ca11fd851166d8a939cad4bb5afb57dd
Sw4gn3m1te <30702991+Sw4gn3m1te@users.noreply.github.com>
2018-07-28 14:03:51 +0200

    Add files via upload

--- cmd_queue.py
+++ cmd_queue.py
@@ -2,20 +2,26 @@ import queue
 import commands
 
 
+
+
 class CommandQueue:
 
+    @commands.exception_logger
     def __init__(self):
 
         self.priority_queue = queue.Queue()
         self.normal_queue = queue.Queue()
         self.next_command = None
 
+    @commands.exception_logger
     def add_to_priority_queue(self, req_dict):
         self.priority_queue.put(req_dict)
 
+    @commands.exception_logger
     def add_to_normal_queue(self, req_dict):
         self.normal_queue.put(req_dict)
 
+    @commands.exception_logger
     def get_next_command(self):
         if self.priority_queue.qsize() > 0:
             return self.priority_queue.get()
@@ -24,6 +30,7 @@ class CommandQueue:
         else:
             return None
 
+    @commands.exception_logger
     def clear_queues(self):
         self.normal_queue.queue.clear()
         self.priority_queue.queue.clear()
